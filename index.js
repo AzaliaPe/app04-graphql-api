f8114233f426ab92871d404be5548964561c46b0
Azalia Peña Hernandez <azaliaphdz@hotmail.com>
2020-10-12 22:44:03 -0700

    Publicar proyecto

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import expressGraphql from 'express-graphql';
 import Scheme from './scheme/Scheme.js';
+import mongoose from 'mongoose';
 
 const {graphqlHTTP} = expressGraphql;
 
 const port = 5000;
 
 const app = express();
+
+const dbName = 'tacos-db';
+const user = 'admin';
+const password = 'tacos27ricos';
+const connectionString = `mongodb+srv://${user}:${password}@clustertacos.y89nv.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(console.log('connected to tacos-db'))
+.catch(error => console.log(`[Error]: ${error}`));
 
 app.use('/graphql', graphqlHTTP({
     schema: Scheme,
